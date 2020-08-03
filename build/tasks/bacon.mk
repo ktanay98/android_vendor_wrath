bf83050b58a7116d6126a1ad0e855aa6ec93f5dd
iamtoxyc <vtechnical1804@gmail.com>
2020-08-03 22:48:38 +0200

    wrath: Add ASCII Logo After Build Done!

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -28,16 +28,26 @@ WRATH_BUILD_TIME :=
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(WRATH_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(WRATH_TARGET_PACKAGE) > $(WRATH_TARGET_PACKAGE).md5sum
-	@echo -e ${CL_RED}"                    Your Build Is Ready! Enjoy the WrathOS                "${CL_RED}
-	@echo -e ${CL_CYN}"=============================-Package Details-============================"${CL_RST}
-	@echo -e ${CL_CYN}"Folder Location: "${CL_MAG} $(WRATH_TARGET_PACKAGE_FOLDER)${CL_RST}
-	@echo -e ${CL_CYN}"ZipName        : "${CL_MAG} $(WRATH_VERSION).zip${CL_RST}
-	@echo -e ${CL_CYN}"MD5            : "${CL_MAG}" $(shell cat $(WRATH_TARGET_PACKAGE).md5sum | awk '{print $$1}')"${CL_RST}
-	@echo -e ${CL_CYN}"Size           : "${CL_MAG}" $(shell du -hs $(WRATH_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
-	@echo -e ${CL_CYN}"Size(in bytes) : "${CL_MAG}" $(shell wc -c $(WRATH_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
-	@echo -e ${CL_CYN}"DateTime       : "${CL_MAG} $(WRATH_BUILD_DATETIME)${CL_RST}
-	@echo -e ${CL_CYN}"Build Type     : "${CL_MAG} $(WRATH_BUILD_TYPE)${CL_RST}
-	@echo -e ${CL_CYN}"==========================================================================="${CL_RST}
+	@echo -e ${CL_RED}""
+	@echo -e ${CL_RED}"========================================================================="${CL_RST}
+	@echo -e ${CL_RED}""
+	@echo -e ${CL_RED}" ██╗    ██╗██████╗  █████╗ ████████╗██╗  ██╗"
+	@echo -e ${CL_RED}" ██║    ██║██╔══██╗██╔══██╗╚══██╔══╝██║  ██║"
+	@echo -e ${CL_RED}" ██║ █╗ ██║██████╔╝███████║   ██║   ███████║"
+	@echo -e ${CL_RED}" ██║███╗██║██╔══██╗██╔══██║   ██║   ██╔══██║"
+	@echo -e ${CL_RED}" ╚███╔███╔╝██║  ██║██║  ██║   ██║   ██║  ██║"
+	@echo -e ${CL_RED}"  ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝"
+	@echo -e ${CL_RED}""
+	@echo -e ${CL_RED}"     Build Is Ready! Fell The Wrath..       "${CL_RED}
+	@echo -e ${CL_RED}"=============================-Package Details-============================"${CL_RST}
+	@echo -e ${CL_RED}"Folder Location: "${CL_RED} $(WRATH_TARGET_PACKAGE_FOLDER)${CL_RST}
+	@echo -e ${CL_RED}"ZipName        : "${CL_RED} $(WRATH_VERSION).zip${CL_RST}
+	@echo -e ${CL_RED}"MD5            : "${CL_RED}" $(shell cat $(WRATH_TARGET_PACKAGE).md5sum | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_RED}"Size           : "${CL_RED}" $(shell du -hs $(WRATH_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_RED}"Size(in bytes) : "${CL_RED}" $(shell wc -c $(WRATH_TARGET_PACKAGE) | awk '{print $$1}')"${CL_RST}
+	@echo -e ${CL_RED}"DateTime       : "${CL_RED} $(WRATH_BUILD_DATETIME)${CL_RST}
+	@echo -e ${CL_RED}"Build Type     : "${CL_RED} $(WRATH_BUILD_TYPE)${CL_RST}
+	@echo -e ${CL_RED}"==========================================================================="${CL_RST}
 	@echo -e ""
 
 wrath: bacon
